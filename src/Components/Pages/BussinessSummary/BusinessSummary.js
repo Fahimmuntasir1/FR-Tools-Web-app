aed5a93c881e8472bda8b772281e3fdacbeac429
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-22 04:07:48 +0600

    business summary section added

--- src/Components/Pages/BussinessSummary/BusinessSummary.js
+++ src/Components/Pages/BussinessSummary/BusinessSummary.js
@@ -2,31 +2,35 @@ import React from "react";
 
 const BussinessSummary = () => {
   return (
-    <div>
-      <h2 className="uppercase">Millions bussiness trust us</h2>
-      <p className="uppercase">try to understand user expection</p>
-      <div class="stats stats-vertical lg:stats-horizontal shadow">
+    <div className="my-5">
+      <h2 className="uppercase text-center text-4xl font-bold text-primary">
+        Millions Business trust us
+      </h2>
+      <p className="uppercase text-center font-semibold text-xl text-slate-500">
+        try to understand user expedition
+      </p>
+      <div class="stats w-full text-center stats-vertical lg:stats-horizontal shadow mt-3">
         <div class="stat">
-          <div class="stat-title">Downloads</div>
-          <div class="stat-value">31K</div>
+          <div class="stat-title">Customers</div>
+          <div class="stat-value text-secondary">10k+</div>
           <div class="stat-desc">Jan 1st - Feb 1st</div>
         </div>
 
         <div class="stat">
-          <div class="stat-title">New Users</div>
-          <div class="stat-value">4,200</div>
+          <div class="stat-title">Annual revenue</div>
+          <div class="stat-value text-secondary">120M+</div>
           <div class="stat-desc">↗︎ 400 (22%)</div>
         </div>
 
         <div class="stat">
-          <div class="stat-title">New Registers</div>
-          <div class="stat-value">1,200</div>
+          <div class="stat-title">Reviews</div>
+          <div class="stat-value text-secondary">33k+</div>
           <div class="stat-desc">↘︎ 90 (14%)</div>
         </div>
 
         <div class="stat">
-          <div class="stat-title">New Registers</div>
-          <div class="stat-value">1,200</div>
+          <div class="stat-title">Tools</div>
+          <div class="stat-value text-secondary">300+</div>
           <div class="stat-desc">↘︎ 90 (14%)</div>
         </div>
       </div>
