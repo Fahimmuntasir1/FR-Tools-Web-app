6c28361a14cd71b2a9598d7c033c40dcda1f4062
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-23 02:35:30 +0600

    single item get api added

--- src/Components/Pages/Purchase.js
+++ src/Components/Pages/Purchase.js
@@ -3,11 +3,15 @@ import { useParams } from "react-router-dom";
 
 const Purchase = () => {
   const { _id } = useParams();
-  const { purchases, setPurchases } = useState({});
+  const [purchase, setPurchase] = useState({});
+  console.log(purchase);
+  const { img } = purchase;
 
-  useEffect(()=> {
-      fetch(``)
-  },[])
+  useEffect(() => {
+    fetch(`http://localhost:5000/tools/${_id}`)
+      .then((res) => res.json())
+      .then((data) => setPurchase(data));
+  }, []);
   return (
     <div>
       <h2>Purchase</h2>
