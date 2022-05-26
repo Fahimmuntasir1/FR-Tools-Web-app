4c04db5731446994172642e944205f44604cb6d5
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-26 22:07:54 +0600

    blog section added ,, issue fixed

--- src/Components/Pages/Blogs.js
+++ src/Components/Pages/Blogs.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = () => {
-    return (
-        <div>
-            <h2>Blogs</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className=" my-5 w-11/12 m-auto">
+        <h1 className="text-xl text-emerald-700 font-semibold mb-3">
+          1. How will you improve the perhtmlFormance of a React Application?
+        </h1>
+        <p>
+          Firebase PerhtmlFormance Monitoring, a real time app perhtmlFormance
+          monitoring tool, helps you keep a close eye on your app as you roll
+          out new features or make configuration changes. PerhtmlFormance
+          Monitoring also gives you control over your perhtmlFormance data with
+          a customizable dashboard that makes it easy to focus on your most
+          important metrics.
+        </p>
+      </div>
+      <div className=" my-5 w-11/12 m-auto">
+        <h1 className="text-xl text-emerald-700 font-semibold mb-3">
+          2. What are the different ways to manage a state in a React
+          application?
+        </h1>
+        <p>
+          There are four main types of state you need to properly manage in your
+          React apps: Local state Global state Server state URL state
+        </p>
+      </div>
+      <div className=" my-5 w-11/12 m-auto">
+        <h1 className="text-xl text-emerald-700 font-semibold mb-3">
+          3. How does prototypical inheritance work?
+        </h1>
+        <p>
+          The Prototypal Inheritance is a feature in javascript used to add
+          methods and properties in objects. It is a method by which an object
+          can inherit the properties and methods of another object.
+          Traditionally, in order to get and set the [[Prototype]] of an object,
+          we use Object. getPrototypeOf and Object.
+        </p>
+      </div>
+      <div className=" my-5 w-11/12 m-auto">
+        <h1 className="text-xl text-emerald-700 font-semibold mb-3">
+          4. What is a unit test? Why should write unit tests?
+        </h1>
+        <p>
+          Unit tests are typically automated tests written and run by software
+          developers to ensure that a section of an application (known as the
+          "unit") meets its design and behaves as intended. In procedural
+          programming, a unit could be an entire module, but it is more commonly
+          an individual function or procedure.
+        </p>
+      </div>
+      <div className=" my-5 w-11/12 m-auto">
+        <h1 className="text-xl text-emerald-700 font-semibold mb-3">
+          5. You have an array of products. Each product has a name, price,
+          description, etc. How will you implement a search to find products by
+          name?
+        </h1>
+        <p>
+          Unit tests are typically automated tests written and run by software
+          developers to ensure that a section of an application (known as the
+          "unit") meets its design and behaves as intended. In procedural
+          programming, a unit could be an entire module, but it is more commonly
+          an individual function or procedure.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
