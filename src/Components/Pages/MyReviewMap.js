5e05886bbc3dd122fa701fc40e26ddeafd428cf6
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2023-02-28 00:04:57 +0600

    working in review section (not finished)

--- src/Components/Pages/MyReviewMap.js
+++ src/Components/Pages/MyReviewMap.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Autoplay, FreeMode, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import MyReviewCard from "./MyReviewCard";
 
 const MyReviewMap = () => {
@@ -18,10 +20,38 @@ const MyReviewMap = () => {
         <div className=" mx-3 bg-slate-400 w-3 card rounded-box place-items-center"></div>
         <div className=" mx-3 bg-slate-400 w-3 card rounded-box place-items-center"></div>
       </div>
-      <div className="grid grid-cols-3 gap-5 my-5">
-        {users?.map((user) => (
-          <MyReviewCard key={user._id} user={user}></MyReviewCard>
-        ))}
+      <div className="p-6 md:px-16">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Autoplay, FreeMode, Pagination]}
+          className="mySwiper "
+        >
+          {users?.map((user) => (
+            <SwiperSlide className="sm:m-10" key={user._id}>
+              <MyReviewCard user={user}></MyReviewCard>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
